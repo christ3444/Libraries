1adc36ebb2de2d547f9eb67192af4cd0b4e2c277
christ3444 <christianmonmail34444@gmail.com>
2022-01-05 08:27:17 +0000

    Initial commit

---
+++ app/src/main/java/com/devchrist/libraries/MainActivity.java
@@ -0,0 +1,16 @@
+package com.devchrist.libraries;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toaster.simpletoast(this,"hello test toast library");
+    }
+}
---
+++ toast/src/main/java/com/devchrist/toast/Toaster.java
@@ -0,0 +1,12 @@
+package com.devchrist.toast;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Toaster {
+
+    public static void simpletoast(Context context, String message){
+
+        Toast.makeText(context, message,Toast.LENGTH_LONG ).show();
+    }
+}
